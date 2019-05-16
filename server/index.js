75ab844af28c29a01dd12c3b5a8c0abfd974da31
Miriam <miriamu88@gmail.com>
2019-05-16 13:15:27 -0500

    backend completed

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const config = require('./db');
+const users = require('./routes/UserRoutes');
 
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
     () => { console.log('Database is connected')},
@@ -10,9 +11,13 @@ mongoose.connect(config.DB, { useNewUrlParser: true }).then(
 );
 
 const app= express();
+app.use(passport.initialize());
+require('./passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+
+app.use('/api/users', users);
 
 app.get('/', function(req, res) {
     res.send('hello');
